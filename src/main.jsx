6d7a0ef9aa42fa79325cc9c8ee16070c64bb79aa
Jibola007 <ajibbola007@gmail.com>
2025-07-24 11:08:45 +0100

    rename

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
-import { CurrencyProvider } from './context/CurrencyContext';
-import { AuthProvider } from './context/AuthContext';
+import { CurrencyProvider } from './context/CurrencyContext.jsx';
+import { AuthProvider } from './context/AuthContext.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
