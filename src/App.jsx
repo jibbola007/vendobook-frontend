a9dabbc97d40b62625e6da449e3e69f1efbc7249
Jibola007 <ajibbola007@gmail.com>
2025-07-24 10:40:22 +0100

    footerupdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
@@ -10,6 +9,8 @@ import EditExpense from './pages/EditExpense';
 import ForgotPassword from './pages/ForgotPassword';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import './index.css';
 import PrivateRoute from './components/PrivateRoute';
 import { useAuth } from './context/AuthContext';
 
@@ -18,50 +19,65 @@ function AppContent() {
   const { user } = useAuth();
 
   const hideNavbarPaths = ['/login', '/signup'];
-
   const shouldShowNavbar = !hideNavbarPaths.includes(location.pathname);
 
   return (
-    <>
+    <div className="app-container">
       {shouldShowNavbar && <Navbar />}
 
-      <Routes>
-        {/* Public Routes */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} /> 
+      <main className="main-content">
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
 
-        {/* Protected Routes */}
-        <Route path="/" element={
-          <PrivateRoute>
-            <Home />
-          </PrivateRoute>
-        } />
-        <Route path="/add" element={
-          <PrivateRoute>
-            <AddExpense />
-          </PrivateRoute>
-        } />
-        <Route path="/history" element={
-          <PrivateRoute>
-            <ExpenseHistory />
-          </PrivateRoute>
-        } />
-        <Route path="/summary" element={
-          <PrivateRoute>
-            <Summary />
-          </PrivateRoute>
-        } />
-        <Route path="/edit/:id" element={
-          <PrivateRoute>
-            <EditExpense />
-          </PrivateRoute>
-        } />
+          {/* Protected Routes */}
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <Home />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/add"
+            element={
+              <PrivateRoute>
+                <AddExpense />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/history"
+            element={
+              <PrivateRoute>
+                <ExpenseHistory />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/summary"
+            element={
+              <PrivateRoute>
+                <Summary />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/edit/:id"
+            element={
+              <PrivateRoute>
+                <EditExpense />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </main>
 
-      </Routes>
-      
-      <Footer/>
-    </>
+      <Footer />
+    </div>
   );
 }
 
